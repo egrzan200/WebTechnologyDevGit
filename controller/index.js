a78780851005c3d912a8bee3d9beb45006a6721f
Egrzan <60653527+egrzan200@users.noreply.github.com>
2020-04-13 13:24:51 +0100

    Added a way to create milestones and linked them to the existing courseworks

--- controller/index.js
+++ controller/index.js
@@ -11,26 +11,34 @@ app.use(bodyParser.urlencoded({
 })); 
 
 //Creates instance of the database
-var DAO = require('./model/nedb');
+var CourseworkDB = require('../model/CourseworkDB');
 //Creates a file for the database
-var dbFile = 'database.nedb.db';
+var dbFile = 'database.nedb.coursework.db';
 //Runs the database in embedded mode
-let dao = new DAO(dbFile);
+let dao = new CourseworkDB(dbFile);
 //Initializes Database, only used during testing.
 //dao.init();
 //dao.deleteAllEntries();
 
+//Creates instance of the milestone database
+var MilestoneDB = require('../model/MilestoneDB');
+//Creates a file for the database
+var milestoneDbFile = 'database.nedb.milestone.db';
+//Runs the database in embedde mode
+let mileDB = new MilestoneDB(milestoneDbFile);
+//Initializes the milestone database with test data
+//mileDB.init();
+
 //Handles mustache
 app.engine('mustache', mustache());
 app.set('view engine', 'mustache');
-app.set('views', path.resolve(__dirname, 'mustache'));
+app.set('views', path.resolve(__dirname, '../views'));
 
 //Main page handler. Passes the list of all database entries to the mustache as "entries"
 app.get('/', function(req,res){
     dao.all()
     .then((list)=>{
         res.render("index",{'entries': list});
-        console.log(list)
     })
     .catch((err)=>{
         res.status(200);
@@ -48,13 +56,47 @@ app.get('/newEntry', function(req,res){
 
 //Handles the post action. Checks if all fields are filled and adds a new object to the database, then redirects to the index page. Or informs the user that fields need to be filled.
 app.post('/newEntry', function(req,res){
-    console.log("I got here");
+    //console.log("I got here");
     if (!req.body.courseworkName|| !req.body.moduleName|| !req.body.dueDate){
         res.status(400).send("Entries must have a coursework name, module and a due date!");
         return;
     }
     dao.add(req.body.courseworkName, req.body.moduleName, req.body.dueDate);
     console.log("A new entry has been added to the database")
+    res.redirect('/');
+});
+
+app.get('/milestones*', function (req,res){
+    var url = req.url;
+    courseworkID = url.slice(12, url.length - 11);
+    //console.log(courseworkID);
+    mileDB.getSpecific(courseworkID).then((milestones)=>{
+        //console.log(milestones);
+        res.render('milestones', {'milestone' : milestones});
+    }).catch((err)=>{
+        res.status(200);
+        res.type('text/plain');
+        res.send('An error has occured while loading the entries from database');
+        console.log('Error: '), 
+        console.log(JSON.stringify(err))
+    })
+});
+
+app.get('/newMilestone', function(req,res){
+    res.render('newMilestone');    
+});
+
+app.post('/newMilestone*', function(req, res){
+    var url = req.url;
+    courseworkID = url.slice(14, url.length - 18);
+    //console.log(courseworkID);
+    
+    if(!req.body.milestoneName || !req.body.milestoneDate || !req.body.milestoneDescription){
+        res.status(400).send("Entries must have a milestone name, date and a description");
+        return;
+    }
+    mileDB.add(req.body.milestoneName, req.body.milestoneDate, req.body.milestoneDescription, courseworkID);
+    console.log("A new entry has been added to the milestone database");
     res.redirect('/');
 });
 
